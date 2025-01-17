41cb8d6325ba8543e324c6b4ad9e9eea092f7c22
tejaswininilakhe <159748008+tejaswininilakhe@users.noreply.github.com>
2025-01-17 11:02:05 +0530

    Update main (2).c

--- main (2).c
+++ main (2).c
@@ -9,9 +9,9 @@ int main(void)
         
     CRC_Init();
         
-    uint8_t ret = CRC_CalculateCRC8(data, 8);
-    uint16_t ret1 = CRC_CalculateCRC16(data, 8);
-    uint32_t ret2 = CRC_CalculateCRC32(data, 8);
+    uint8_t ret = CRC_CalculateCRC8(data, 8); //calculate 8 bit data
+    uint16_t ret1 = CRC_CalculateCRC16(data, 8); //calculate 16 bit data
+    uint32_t ret2 = CRC_CalculateCRC32(data, 8); //calculate 32 bit data
         
     printf("CRC8 : 0x%X\n", ret);
     printf("CRC16: 0x%X\n", ret1);
